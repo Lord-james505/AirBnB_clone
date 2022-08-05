80dba63a41a67b221aaf20774eda3e88f9d8fc19
Lord-james505 <adeagbojames3991@gmail.com>
2022-08-05 08:15:46 +0000

    test directory

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""
+Created on Mon Aug 08 20:38:00 2022
+
+@author: ADEAGBO Opeyemi
+         Victor Chidi
+"""
---
+++ tests/test_console.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import unittest
+import pycodestyle
+
+class TestHBNBCommand(unittest.TestCase):
+        """
+        class for testing HBNBCommand class' methods
+        """
