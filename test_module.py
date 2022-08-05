df256469a704a28ce16bec42dd83cfbb0d8243e7
Lord-james505 <adeagbojames3991@gmail.com>
2022-08-05 07:01:00 +0000

    task one

---
+++ test_module.py
@@ -0,0 +1,30 @@
+def add(a: int, b: int) -> float:
+    """
+    This function add two integers
+    Return Float
+    """
+    return a+b
+
+
+def sub(a: int, b: int) -> float:
+    """
+    This function subtract two integers
+    Return Float
+    """
+    return a-b
+
+
+def multiply(a: int, b: int) -> float:
+    """
+    This function multiply two integers
+    Return Float
+    """
+    return a*b
+
+
+def div(a: int, b: int) -> float:
+    """
+    This function divide two integers
+    Return Float
+    """
+    return a/b
